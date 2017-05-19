69285fd97414cc62b0f11d20d88683b164ba5635
Zakeria Ali <zak.fined@gmail.com>
2017-05-19 18:04:39 +0100

    Update the DisplayLang component to a Stateless Functional component

--- components/DisplayLang.js
+++ components/DisplayLang.js
@@ -4,25 +4,22 @@ var PropTypes = require('prop-types');
 // This file contains a presentational component which only
 // takes in props from a NavBar component and renders it to the view
 
-class DisplayLang extends React.Component {
-  render(){
-    var langList = ['All','Javascript','Ruby','CSS','Python','Java', 'PHP'];
-    return (
-      <div>
-        <ul className="lists">
-          {langList.map(function(langItem){
-            return (
-              <li
-                style={langItem === this.props.currentLang ? {color: 'coral'} : null}
-                onClick={this.props.changeLanguage.bind(null, langItem)}
-                key={langItem}>
-              {langItem}</li>
-            )
-          }, this)}
-        </ul>
-      </div>
-    )
-  }
+function DisplayLang(props){
+  return (
+    <div>
+      <ul className="lists">
+        {langList.map(function(langItem){
+          return (
+            <li
+              style={langItem === props.currentLang ? {color: 'coral'} : null}
+              onClick={props.changeLanguage.bind(null, langItem)}
+              key={langItem}>
+            {langItem}</li>
+          )
+        }, this)}
+      </ul>
+    </div>
+  )
 }
 
 
