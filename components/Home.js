10c1e89e60b04d973b047cc664ad4eb838323144
Zakeria Ali <zak.fined@gmail.com>
2017-05-22 21:24:39 +0100

    Add the typing effect on the Home component

--- components/Home.js
+++ components/Home.js
@@ -1,11 +1,12 @@
 var React = require('react');
 var Link = require('react-router-dom').Link;
+var Rewrite = require('react-rotating-text');
 
 class Home extends React.Component {
   render(){
     return (
       <div className="homepage">
-        <h1>Explore and compare Github repositories</h1>
+        <h1>Explore and <Rewrite cursor={false} typingInterval={190} items={['compare', 'battle']}/> Github repositories</h1>
         <Link to='/battle' className="button">
           Compare
         </Link>
