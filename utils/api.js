3955fd0b4456e670a0f7642fad3697edc6956357
Zakeria Ali <zak.fined@gmail.com>
2017-05-29 20:37:15 +0100

    Create function to get user profile from Github

--- utils/api.js
+++ utils/api.js
@@ -1,9 +1,22 @@
 var axios = require('axios');
 
+var clientID = '5e72fcc8c9cdeaa5de2a';
+var secretID = 'eadc203e71f8aa11aaad1e541d6c2b3472ccd9fb';
+var params = '?client_id=' + client_id + '&client_secret=' + secretID;
+
+function getProfile(username){
+  return axios.get('https://github.com/users/' + username + params)
+    .then(function(user){
+      return user.data;
+    });
+}
+
 
 module.exports = {
+
   fetchData: function(lang){
-    var encodedData = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:' + lang + '&sort=stars&order=desc');
+    var encodedData = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:' + lang
+    + '&sort=stars&order=desc');
 
     return axios.get(encodedData).then(function(response){
         return response.data.items;
